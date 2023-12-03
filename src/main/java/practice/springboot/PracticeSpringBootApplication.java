104b3310452af43c5f78f65b80ecdbbc8be23c82
zserinz <serinn1023@gmail.com>
2023-12-04 00:07:26 +0900

    initializr spring boot!

---
+++ src/main/java/practice/springboot/PracticeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package practice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practice/springboot/PracticeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
