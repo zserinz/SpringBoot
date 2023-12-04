4ff1e7c5dc4e9a0237186c69670dedecdb45ce41
zserinz <serinn1023@gmail.com>
2023-12-04 22:51:11 +0900

    [토비] 독립 실행형 서블릿 애플리케이션 만들어보기

--- src/main/java/practice/springboot/HelloController.java
+++ src/main/java/practice/springboot/HelloController.java
@@ -3,9 +3,7 @@ package practice.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
-    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
--- src/main/java/practice/springboot/PracticeSpringBootApplication.java
+++ src/main/java/practice/springboot/PracticeSpringBootApplication.java
@@ -1,13 +1,59 @@
 package practice.springboot;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class PracticeSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PracticeSpringBootApplication.class, args);
-    }
+        // 내장형 톰캣의 초기화 작업과 간편한 설정을 지원하도록 스프링 부트가 제공하는 라이브러리가 존재한다.
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        // 서블릿 컨테이너 생성
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
 
+            servletContext.addServlet("frontController", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능
+
+                    // 1. 매핑 진행
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        // 2. 바인딩 진행, 컨트롤러 처리에 필요한 사전 작업들이다.
+                        String name = req.getParameter("name");
+
+                        // 실제 요청을 받아서 컨트롤러 기능 실행
+                        String ret = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    }
+                    else {
+                        //
+                    }
+                }
+                // url mapping
+            }).addMapping("/*");
+        });
+        webServer.start();
+    }
 }
