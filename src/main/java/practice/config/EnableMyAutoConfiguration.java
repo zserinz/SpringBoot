a964973fd6a78ae066de6f3e7af29bd72d440be4
zserinz <serinn1023@gmail.com>
2023-12-13 23:40:05 +0900

    [토비] 동적 자동 구성 정보 등록

--- src/main/java/practice/config/EnableMyAutoConfiguration.java
+++ src/main/java/practice/config/EnableMyAutoConfiguration.java
@@ -1,8 +1,6 @@
 package practice.config;
 
 import org.springframework.context.annotation.Import;
-import practice.config.autoconfig.DispatcherServletConfig;
-import practice.config.autoconfig.TomcatWebServerConfig;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class}) // Config 설정 파일을 주입시키기 위해 작성 (1개 이상의 configure를 관리할 때에도 사용)
+@Import(MyAutoConfigImportSelector.class) // 직접 클래스를 하드코딩해서 넣지 않고, 동적으로 String[]에서 관리될 수 있도록 한다.
 public @interface EnableMyAutoConfiguration {
 }
